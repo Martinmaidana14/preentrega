a4823ab71f5a4d9d743db02cce643913251abfe7
MartinMaidana <m14maidana@gmail.com>
2023-10-02 23:43:45 -0300

    para-preEntrega3

--- js/carrito.js
+++ js/carrito.js
@@ -4,11 +4,11 @@ class Persona {
 	
 		this.apellido = apellido;
 	}
-	
+	//metodo estatico para reutilizar constructor, truquito doble constructor
 	static nuevaPersona(objeto) {
 		return new Persona(objeto.nombre, objeto.apellido);
 	}
-	
+	//recuperar estructura de la clase 
 	mostrar_nombre_completo() {
 		return this.nombre + " " + this.apellido;
 	}
@@ -71,58 +71,7 @@ function agregarAlCarrito(id){
 }
 
 
-//Transformar un objeto JS a un string de formato JSON
 
-/*
-const producto1 = { id: 2, producto: "Arroz" };
-const enJSON    = JSON.stringify(producto1);
-
-console.log(enJSON); // {"id":2,"producto":"Arroz"}
-console.log(typeof producto1); // object
-console.log(typeof enJSON);    // string
-
-localStorage.setItem("producto1", enJSON);
-// Se guarda {"id":2,"producto":"Arroz"}
-*/
-
-//Almacenar array completo
-//guardarLocal("listaProductos", JSON.stringify(productos));
-
-
-//Bienvenida de Usuario ??
-/*
-class Persona {
-constructor(nombre, apellido) {
-    this.nombre = nombre;
-
-    this.apellido = apellido;
-}
-
-static nuevaPersona(objeto) {
-    return new Persona(objeto.nombre, objeto.apellido);
-}
-
-mostrar_nombre_completo() {
-    return this.nombre + " " + this.apellido;
-}
-}
-
-let nombre = prompt("ingrese su nombre");
-
-let apellido = prompt("ingrese su apellido");
-
-//localStorage.setItem("nombre-completo",nombre + " " + apellido);
-
-let persona = new Persona(nombre, apellido);
-
-localStorage.setItem("persona", JSON.stringify(persona));
-
-let objeto_persona = JSON.parse(localStorage.getItem("persona"));
-
-let p = Persona.nuevaPersona(objeto_persona);
-
-alert("Bienvenid@ " + p.mostrar_nombre_completo());
-*/
 
 
 
