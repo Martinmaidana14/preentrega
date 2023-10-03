da631437ae2bd94e441228d4637f87fbfa4573ce
MartinMaidana <m14maidana@gmail.com>
2023-10-02 21:00:35 -0300

    correcciones

--- js/carrito.js
+++ js/carrito.js
@@ -18,7 +18,7 @@ const mostrarProductos = (data) => {
 	const btnComprar = document.querySelectorAll('.btn-compra'); //Nodelist[]
 	btnComprar.forEach(el => {
 		el.addEventListener('click', (e) =>{ //Agregar Evento
-			agregarAlCarrito(e.target.id)
+			agregarAlCarrito(e.target.id);
 		});
 	});
 }
@@ -29,14 +29,32 @@ const carrito = [];
 
 function agregarAlCarrito(id){
 	//console.log(id)
+	carrito.some()
 
 	let prodEncontrado = productos.find( prod => prod.id === parseInt(id)); //para encontrar un producto, recorriendo array 
 	//console.log(prodEncontrado) me devuelve el producto que clickeo y asi poder encontrarlos 
-	
+
 	carrito.push(prodEncontrado);
 	console.log(carrito);
 }
 
+
+//Transformar un objeto JS a un string de formato JSON
+
+/*
+const producto1 = { id: 2, producto: "Arroz" };
+const enJSON    = JSON.stringify(producto1);
+
+console.log(enJSON); // {"id":2,"producto":"Arroz"}
+console.log(typeof producto1); // object
+console.log(typeof enJSON);    // string
+
+localStorage.setItem("producto1", enJSON);
+// Se guarda {"id":2,"producto":"Arroz"}
+*/
+
+//Almacenar array completo
+//guardarLocal("listaProductos", JSON.stringify(productos));
 
 
 
